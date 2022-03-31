e4f2cfb0c84d9b1b1aa142feede7c981626d1248
Widyadk13 <widyadwikusuma18@gmail.com>
2022-03-31 21:14:33 +0500

    Add Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,14 @@
 import React from 'react';
 import './App.css';
-import Bookslist from './components/booksList';
-import Navbar from './components/Navbar'
-import Header from './components/Header';
-import Footer from './components/footer';
-import Login from './components/Login';
+import Navbar from './components/Navbar';
+
 
 
 function App() {
   return (
     <div className="App">
-      <Login/>
-      <Navbar/>
-      <Header/>
-      <Bookslist/>
-      <Footer/>
-     </div>
+      <Navbar />
+    </div>
   );
 }
 
