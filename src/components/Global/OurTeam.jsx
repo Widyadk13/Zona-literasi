28db4a8527d44dbbfd53ba7b5250f96090621a7f
Dandyndot29 <dandysetiawanptr@gmail.com>
2022-04-10 12:52:56 +0800

    addlink medsos edit logout

--- src/components/Global/OurTeam.jsx
+++ src/components/Global/OurTeam.jsx
@@ -15,120 +15,121 @@ const Teams = () => {
       </div>
       
       <div className="foto">
-        <div className="">
+      <div className="">
           <div className="team-member">
-            <img className="mx-auto rounded-circle" src="https://fashionsista.co/wallpaper/wallpaper/20210811/foto-cowok-keren-buat-profil-fb-foto-foto-keren-preview.jpg" width="100px" height="150px" alt=""/>
-            <h4 className="text-center">Dandy Setiawan</h4>
-            <p className="text-muted">Lead Designer</p>
-            <ul className="list-inline social-buttons">
-              <li className="list-inline-item">
-                <a href="#something">
-                  <i className="fa-brands fa-twitter fa-2x"></i>
-                </a>
-              </li>
-              <li className="list-inline-item">
-                <a href="#something">
-                  <i className="fa-brands fa-facebook fa-2x"></i>
-                </a>
-              </li>
-              <li className="list-inline-item">
-                <a href="#something">
-                  <i className="fa-brands fa-instagram-square fa-2x"></i>
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-        <div className="">
-          <div className="team-member">
-            <img className="mx-auto rounded-circle" src="https://fashionsista.co/wallpaper/wallpaper/20210811/foto-cowok-keren-buat-profil-fb-foto-foto-keren-preview.jpg" width="100px" height="150px" alt=""/>
-            <h4 className="text-center">Muhammad Rifky</h4>
-            <p className="text-muted">Lead Designer</p>
-            <ul className="list-inline social-buttons">
-              <li className="list-inline-item">
-                <a href="#something">
-                  <i className="fa-brands fa-twitter fa-2x"></i>
-                </a>
-              </li>
-              <li className="list-inline-item">
-                <a href="#something">
-                  <i className="fa-brands fa-facebook fa-2x"></i>
-                </a>
-              </li>
-              <li className="list-inline-item">
-                <a href="#something">
-                  <i className="fa-brands fa-instagram-square fa-2x"></i>
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-        <div className="">
-          <div className="team-member">
-            <img className="mx-auto rounded-circle" src="https://i.pinimg.com/originals/16/a7/22/16a722d6a5881d8fd6b24ac3ea32dd0e.jpg" width="100px" height="150px" alt=""/>
+            <img className="mx-auto rounded-circle" src="https://files.fm/thumb_show.php?i=p3ugvq942" width="100px" height="150px" alt=""/>
             <h4 className="text-center">Widya Dwi Kusuma</h4>
-            <p className="text-muted">Lead Marketer</p>
-            <ul className="list-inline social-buttons">
-              <li className="list-inline-item">
-              <a href="#something">
-                  <i className="fa-brands fa-twitter fa-2x"></i>
-                </a>
-              </li>
-              <li className="list-inline-item">
-                <a href="#something">
-                  <i className="fa-brands fa-facebook fa-2x"></i>
-                </a>
-              </li>
-              <li className="list-inline-item">
-                <a href="#something">
-                  <i className="fa-brands fa-instagram-square fa-2x"></i>
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
-        <div className="">
-          <div className="team-member">
-            <img className="mx-auto rounded-circle" src="https://cf.shopee.co.id/file/425257bba66f9714f22f34594dfbb645"width="100px" height="150px" alt=""/>
-            <h4 className="text-center">Prindayu Arum</h4>
             <p className="text-muted">Lead Developer</p>
             <ul className="list-inline social-buttons">
               <li className="list-inline-item">
-              <a href="#something">
+              <a href="https://instagram.com/dwiwidyakusuma?igshid=YmMyMTA2M2Y=">
                   <i className="fa-brands fa-twitter fa-2x"></i>
                 </a>
               </li>
               <li className="list-inline-item">
-                <a href="#something">
+                <a href="https://instagram.com/dwiwidyakusuma?igshid=YmMyMTA2M2Y=">
                   <i className="fa-brands fa-facebook fa-2x"></i>
                 </a>
               </li>
               <li className="list-inline-item">
-                <a href="#something">
+                <a href="https://instagram.com/dwiwidyakusuma?igshid=YmMyMTA2M2Y=">
                   <i className="fa-brands fa-instagram-square fa-2x"></i>
                 </a>
               </li>
             </ul>
           </div>
         </div>
-      <div className="">
+        <div className="">
           <div className="team-member">
-            <img className="mx-auto rounded-circle" src="https://images.tokopedia.net/img/cache/500-square/product-1/2019/9/22/17497718/17497718_d1896fc4-867e-4564-b481-623800c33341_835_835" width="100px" height="150px" alt=""/>
+            <img className="mx-auto rounded-circle" src="https://files.fm/thumb_show.php?i=dgmng3qsf" width="100px" height="150px" alt=""/>
+            <h4 className="text-center">Dandy Setiawan</h4>
+            <p className="text-muted">Lead Marketer</p>
+            <ul className="list-inline social-buttons">
+              <li className="list-inline-item">
+                <a href="https://twitter.com/dandy_ndott">
+                  <i className="fa-brands fa-twitter fa-2x"></i>
+                </a>
+              </li>
+              <li className="list-inline-item">
+                <a href="https://m.facebook.com/dandysetiyawanputra">
+                  <i className="fa-brands fa-facebook fa-2x"></i>
+                </a>
+              </li>
+              <li className="list-inline-item">
+                <a href="https://instagram.com/dandysetiawanp?igshid=YmMyMTA2M2Y=">
+                  <i className="fa-brands fa-instagram-square fa-2x"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="">
+          <div className="team-member">
+            <img className="mx-auto rounded-circle" src="https://files.fm/thumb_show.php?i=ga8m4eymc" width="100px" height="150px" alt=""/>
+            <h4 className="text-center">Muhammad Rifky</h4>
+            <p className="text-muted">Lead Editor</p>
+            <ul className="list-inline social-buttons">
+              <li className="list-inline-item">
+                <a href="https://instagram.com/rifkikhadafi_?igshid=YmMyMTA2M2Y=">
+                  <i className="fa-brands fa-twitter fa-2x"></i>
+                </a>
+              </li>
+              <li className="list-inline-item">
+                <a href="https://instagram.com/rifkikhadafi_?igshid=YmMyMTA2M2Y=g">
+                  <i className="fa-brands fa-facebook fa-2x"></i>
+                </a>
+              </li>
+              <li className="list-inline-item">
+                <a href="https://instagram.com/rifkikhadafi_?igshid=YmMyMTA2M2Y=">
+                  <i className="fa-brands fa-instagram-square fa-2x"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="">
+          <div className="team-member">
+            <img className="mx-auto rounded-circle" src="https://files.fm/thumb_show.php?i=zpcg7hcwa"width="100px" height="150px" alt=""/>
             <h4 className="text-center">Imam Apriyan</h4>
             <p className="text-muted">Lead Community</p>
             <ul className="list-inline social-buttons">
               <li className="list-inline-item">
-              <a href="#something">
+              <a href="https://instagram.com/imam_sg04?igshid=YmMyMTA2M2Y=">
                   <i className="fa-brands fa-twitter fa-2x"></i>
                 </a>
               </li>
               <li className="list-inline-item">
-                <a href="#something">
+                <a href="https://instagram.com/imam_sg04?igshid=YmMyMTA2M2Y=">
                   <i className="fa-brands fa-facebook fa-2x"></i>
                 </a>
               </li>
               <li className="list-inline-item">
-                <a href="#something">
+                <a href="https://instagram.com/imam_sg04?igshid=YmMyMTA2M2Y=">
+                  <i className="fa-brands fa-instagram-square fa-2x"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        
+      <div className="">
+          <div className="team-member">
+            <img className="mx-auto rounded-circle" src="https://files.fm/thumb_show.php?i=ava4huptm" width="100px" height="150px" alt=""/>
+            <h4 className="text-center">Prindayu Arum</h4>
+            <p className="text-muted">Lead Designer</p>
+            <ul className="list-inline social-buttons">
+              <li className="list-inline-item">
+              <a href="https://instagram.com/prindayu_arum?igshid=YmMyMTA2M2Y=">
+                  <i className="fa-brands fa-twitter fa-2x"></i>
+                </a>
+              </li>
+              <li className="list-inline-item">
+                <a href="https://instagram.com/prindayu_arum?igshid=YmMyMTA2M2Y=">
+                  <i className="fa-brands fa-facebook fa-2x"></i>
+                </a>
+              </li>
+              <li className="list-inline-item">
+                <a href="https://instagram.com/prindayu_arum?igshid=YmMyMTA2M2Y=">
                   <i className="fa-brands fa-instagram-square fa-2x"></i>
                 </a>
               </li>
